1718dbf42925440a853612489950ecd00b01ce4d
Eugene Theriault <hybridbassist@gmail.com>
2021-12-02 16:07:12 -0800

    Complete part 2 of Day 2

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -8,24 +8,35 @@ fn main() {
 
         let _result = buf_reader.read_to_string(&mut contents); 
 
-        let mut x_coordinate: i32 = 0;
-        let mut y_coordinate: i32 = 0;
+        let mut x_coordinate: i64 = 0;
+        let mut y_coordinate: i64 = 0;
+        let mut aim: i64 = 0;
+        let mut depth: i64 = 0;
 
         let input = contents.trim().split("\n");
 
         for line in input {
             let chars: Vec<&str> = line.trim().split(" ").collect();
             let first_letter: &str = chars[0];
-            let num: i32 = chars[chars.len() - 1].parse::<i32>().unwrap();
-            println!("direction: {}, number: {}", first_letter, num);
+            let num: i64 = chars[chars.len() - 1].parse::<i64>().unwrap();
             match first_letter {
-                "forward" => x_coordinate += num,
-                "up" => y_coordinate -= num,
-                "down" => y_coordinate += num,
+                "forward" => {
+                    x_coordinate += num;
+                    depth += aim * num;
+                },
+                "up" => {
+                    y_coordinate -= num;
+                    aim -= num;
+                },
+                "down" => {
+                    y_coordinate += num;
+                    aim += num;
+                },
                 _ => println!("Something went wrong"),
             }
         }
 
-        println!("The final position is: {}", x_coordinate * y_coordinate)
+        println!("The final position is: {}", x_coordinate * y_coordinate);
+        println!("The final position with aiming is: {}", x_coordinate * depth);
     }
 }
